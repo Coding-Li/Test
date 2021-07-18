37a28fdbe4517829deca343ebdfe96a5b69dcdce
lijunhao <lijunhaoit@163.com>
2021-07-18 11:26:50 +0800

    init
    init

---
+++ test-lijunhaoit/src/main/java/cn/lijunhaoit/test/collection/ListTest.java
@@ -0,0 +1,36 @@
+package cn.lijunhaoit.test.collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * List是最基础的集合，一种有序列表
+ * ArrayList，LinkList
+ * @author lijun
+ */
+public class ListTest {
+    public static void main(String[] args) {
+
+        int i = 10 >> 1;
+        /*
+        ArrayList
+        ArrayList有两个重要的属性，elementData和DEFAULT_CAPACITY，
+        elementData：是一个Object数组用于保存我们向list中添加的数据
+        DEFAULT_CAPACITY：初始化位数10，代表当我们new ArrayList() 的时候，实际上是初始化我们的elementData数组 大小为10
+         */
+        List arrayList = new ArrayList();
+
+        /*
+        初始化只能从10个开始吗？
+        其实并不然，工具已经给我们了一个带参数的构造方法，这里我们创建一个初始化集合大小为11的ArrayList
+         */
+        List arrayListNum11 = new ArrayList(11);
+
+        //LinkedList
+        List linkedList = new LinkedList();
+
+        //ArrayList在末尾添加一个
+        arrayList.add("addOne");
+    }
+}
---
+++ test-lijunhaoit/src/main/java/cn/lijunhaoit/test/reflex/ClassDemo.java
@@ -0,0 +1,25 @@
+package cn.lijunhaoit.test.reflex;
+
+/**
+ * 谈到反射第一个想到的就是Object对象和java.lang.Class对象
+ * @author lijun
+ */
+public class ClassDemo {
+    public static void main(String[] args) {
+        //第一种获取方法,通过class静态变量获取
+        Class clazz = String.class;
+
+        //第二种获取方法，通过对象的getClass()方法获取
+        String s = "Hello";
+        Class clazzGetClass = s.getClass();
+
+        //第三种获取方法，通过Class.forName()获取,同时可能会报一个ClassNotFound的异常
+        try {
+            Class clazzForName = Class.forName("java.lang.String");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
